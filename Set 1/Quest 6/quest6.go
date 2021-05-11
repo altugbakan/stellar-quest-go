8002ae012fcfbb6ee2c089045d3febb61fa9817f
altugbakan <43248015+altugbakan@users.noreply.github.com>
2021-05-11 18:48:59 +0300

    Add files via upload
    Added the solution for Set 1 - Quest 6

---
+++ Set 1/Quest 6/quest6.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
+)
+
+func main() {
+	// Enter your quest account secret key, asset name and
+	// the issuer address you created on Quest 5 below.
+	secret := "SAGXUH5I7IMSDT6RLCF7HSP4UISLYTF6FVAYLTRYX6KSNLJQN266JHPK"
+	assetName := "CSTM"
+	issuerAddress := "GDYXSHI7CZLRUE3CVISMYN7XB2Q7OW5HUT7RMX3W5PJE4PPA4GH5ZVTC"
+	// ..........................................
+
+	// Get the keypair of the quest account from the secret key.
+	questAccount, _ := keypair.Parse(secret)
+
+	// Fetch the quest account from the network.
+	client := horizonclient.DefaultTestNetClient
+	ar := horizonclient.AccountRequest{AccountID: questAccount.Address()}
+	sourceAccount, err := client.AccountDetail(ar)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Create the asset
+	asset := txnbuild.CreditAsset{
+		Code:   assetName,
+		Issuer: issuerAddress,
+	}
+
+	// Build a sell offer operation.
+	op := txnbuild.ManageSellOffer{
+		Selling: asset,
+		Buying:  txnbuild.NativeAsset{},
+		Amount:  "0.1",
+		Price:   "0.1",
+	}
+
+	// Construct the transaction.
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &sourceAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&op},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction.
+	tx, err = tx.Sign(network.TestNetworkPassphrase, questAccount.(*keypair.Full))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network.
+	status, err := client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response.
+	fmt.Println(status)
+}
