95ab4653df707c5845ca7140afa0916f5decf854
altugbakan <43248015+altugbakan@users.noreply.github.com>
2021-05-11 15:03:25 +0300

    Add files via upload
    Added the solution for Set 1 - Quest 2

---
+++ Set 1/Quest 2/quest2.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
+)
+
+func main() {
+	// Enter your quest account secret key below.
+	secret := "SAGXUH5I7IMSDT6RLCF7HSP4UISLYTF6FVAYLTRYX6KSNLJQN266JHPK"
+	// ..........................................
+
+	// Get the keypair of the quest account from the secret key.
+	questAccount, _ := keypair.Parse(secret)
+
+	// Generate a random testnet account.
+	pair, err := keypair.Random()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("The generated secret key is %v\n", pair.Seed())
+	fmt.Printf("The generated public key is %v\n", pair.Address())
+
+	// Fund and create the generated account.
+	resp, err := http.Get("https://friendbot.stellar.org/?addr=" + pair.Address())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get and print the response from friendbot.
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+
+	// Fetch the quest account from the network
+	client := horizonclient.DefaultTestNetClient
+	ar := horizonclient.AccountRequest{AccountID: questAccount.Address()}
+	sourceAccount, err := client.AccountDetail(ar)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Build a payment creation operation.
+	op := txnbuild.Payment{
+		Destination: pair.Address(),
+		Asset:       txnbuild.NativeAsset{},
+		Amount:      "10",
+	}
+
+	// Construct the transaction
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &sourceAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&op},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction
+	tx, err = tx.Sign(network.TestNetworkPassphrase, questAccount.(*keypair.Full))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network
+	status, err := client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response
+	fmt.Println(status)
+}
