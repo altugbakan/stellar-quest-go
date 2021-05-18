cfba66ce57eb3cb2fa9b1acfcc0deec2c7d6afc7
Altuğ Bakan <43248015+altugbakan@users.noreply.github.com>
2021-05-18 16:10:17 +0300

    Add files via upload
    Added the solution for Set 3 - Quest 5

---
+++ Set 3/Quest 5/quest5.go
@@ -0,0 +1,228 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
+)
+
+func main() {
+	// Get the secret key from user input.
+	var secret string
+	fmt.Printf("Please enter your secret key: ")
+	fmt.Scanln(&secret)
+
+	// Get the keypair of the quest account from the secret key.
+	questAccount, _ := keypair.Parse(secret)
+
+	// Generate a random testnet account.
+	pair, err := keypair.Random()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The generated secret key is %v\n", pair.Seed())
+	fmt.Printf("The generated public key is %v\n", pair.Address())
+
+	// Fund and create the quest account.
+	resp, err := http.Get("https://friendbot.stellar.org/?addr=" + questAccount.Address())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get and print the response from friendbot.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp.Body.Close()
+	fmt.Println("Friendbot response:")
+	fmt.Println(string(body))
+
+	// Fund and create the generated account.
+	resp, err = http.Get("https://friendbot.stellar.org/?addr=" + pair.Address())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get and print the response from friendbot.
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Friendbot response:")
+	fmt.Println(string(body))
+
+	// Fetch the quest account from the network.
+	client := horizonclient.DefaultTestNetClient
+	ar := horizonclient.AccountRequest{AccountID: questAccount.Address()}
+	issuerAccount, err := client.AccountDetail(ar)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Fetch the generated account from the network.
+	ar = horizonclient.AccountRequest{AccountID: pair.Address()}
+	generatedAccount, err := client.AccountDetail(ar)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Build a set options operation to allow clawbacks from the issuer.
+	allowOp := txnbuild.SetOptions{
+		// Note that the revocable flag must be set to enable clawbacks.
+		SetFlags: []txnbuild.AccountFlag{txnbuild.AuthRevocable, txnbuild.AuthClawbackEnabled},
+	}
+
+	// Construct the transaction from the issuer account.
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &issuerAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&allowOp},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction.
+	tx, err = tx.Sign(network.TestNetworkPassphrase, questAccount.(*keypair.Full))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network.
+	status, err := client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response.
+	fmt.Printf("Successfully submitted transaction!\nTransaction ID: %v\n", status.ID)
+
+	// Create the asset
+	asset := txnbuild.CreditAsset{
+		// The asset code was hidden in the hint.
+		Code:   "MariusLenk",
+		Issuer: questAccount.Address(),
+	}
+
+	// Build a change trust operation to allow assets from the issuer.
+	trustOp := txnbuild.ChangeTrust{
+		Line: asset,
+	}
+
+	// Construct the transaction from the generated account.
+	tx, err = txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &generatedAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&trustOp},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction.
+	tx, err = tx.Sign(network.TestNetworkPassphrase, pair)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network.
+	status, err = client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response.
+	fmt.Printf("Successfully submitted transaction!\nTransaction ID: %v\n", status.ID)
+
+	// Build a payment operation.
+	paymentOp := txnbuild.Payment{
+		Destination: pair.Address(),
+		Amount:      "100",
+		Asset:       asset,
+	}
+
+	// Construct the transaction from the issuer account.
+	tx, err = txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &issuerAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&paymentOp},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction.
+	tx, err = tx.Sign(network.TestNetworkPassphrase, questAccount.(*keypair.Full))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network.
+	status, err = client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response.
+	fmt.Printf("Successfully submitted transaction!\nTransaction ID: %v\n", status.ID)
+
+	// Build a clawback operation.
+	clawbackOp := txnbuild.Clawback{
+		From:   pair.Address(),
+		Amount: "50",
+		Asset:  asset,
+	}
+
+	// Construct the transaction from the issuer account.
+	tx, err = txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &issuerAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&clawbackOp},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction.
+	tx, err = tx.Sign(network.TestNetworkPassphrase, questAccount.(*keypair.Full))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network.
+	status, err = client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response.
+	fmt.Printf("Successfully submitted transaction!\nTransaction ID: %v\n", status.ID)
+
+	// Wait for user input to exit.
+	fmt.Println("Press \"Enter\" to exit.")
+	fmt.Scanln()
+}
