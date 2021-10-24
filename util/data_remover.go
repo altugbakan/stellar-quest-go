1408e5cb8ffc5519763c5048ac27d0c9d0fcdda0
altugbakan <altugbakan@gmail.com>
2021-10-24 12:15:46 +0300

    Added data remover for utility.

---
+++ util/data_remover.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
+)
+
+func main() {
+	// Get the secret key from user input.
+	var secret string
+	fmt.Printf("Please enter your secret key: ")
+	fmt.Scanln(&secret)
+
+	// Get the keypair of the quest account from the secret key.
+	questAccount, _ := keypair.Parse(secret)
+
+	// Fetch the account from the network.
+	client := horizonclient.DefaultTestNetClient
+	ar := horizonclient.AccountRequest{AccountID: questAccount.Address()}
+	sourceAccount, err := client.AccountDetail(ar)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Get the data from the network.
+	resp, err := http.Get("https://horizon-testnet.stellar.org/accounts/" + questAccount.Address())
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp.Body.Close()
+
+	// Parse the response.
+	var response map[string]map[string]string
+	json.Unmarshal(body, &response)
+
+	// Build manage data operations.
+	var ops []txnbuild.Operation
+	for data := range response["data"] {
+		ops = append(ops, &txnbuild.ManageData{
+			Name: data,
+		})
+	}
+
+	// Construct the transaction.
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &sourceAccount,
+			IncrementSequenceNum: true,
+			Operations:           ops,
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewTimeout(300),
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Sign the transaction.
+	tx, err = tx.Sign(network.TestNetworkPassphrase, questAccount.(*keypair.Full))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Send the transaction to the network.
+	status, err := client.SubmitTransaction(tx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Print the response.
+	fmt.Printf("Successfully submitted transaction!\nTransaction ID: %v\n", status.ID)
+
+	// Wait for user input to exit.
+	fmt.Println("Press \"Enter\" to exit.")
+	fmt.Scanln()
+}
