fb7851b858bd939193af2f914d52fab335b49239
altugbakan <altugbakan@gmail.com>
2021-11-26 00:28:00 +0300

    Fixed the solution for Series 4 - Quest 5.

--- series4/quest5/solution.go
+++ series4/quest5/solution.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
-	"github.com/stellar/go/network"
 	"github.com/stellar/go/txnbuild"
 )
 
@@ -58,77 +57,11 @@ func main() {
 		log.Fatal(err)
 	}
 	sponsoredAccount := accounts.Embedded.Records[0].AccountID
-	var sponsoredSigner string
-	for _, signer := range accounts.Embedded.Records[0].Signers {
-		if signer.Sponsor == public {
-			sponsoredSigner = signer.Key
-		}
-	}
-	if sponsoredSigner == "" {
-		log.Fatal("no signer found")
-	}
 
-	// We'll cheat a bit here lol
-	//---------------------------------CHEAT-START------------------------------------
-	// Build a payment operation.
-	paymentOp := txnbuild.Payment{
-		Destination: sponsoredAccount,
-		Amount:      "5",
-		Asset:       txnbuild.NativeAsset{},
-	}
-
-	// Fetch the account from the network.
-	generatedAccount, err := client.AccountDetail(horizonclient.AccountRequest{
-		AccountID: generatedKp.Address(),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Send the payment operation from the friendbot funded account,
-	// as the quest account does not have enough XLM to revoke sponsorship.
-	tx, err := txnbuild.NewTransaction(
-		txnbuild.TransactionParams{
-			SourceAccount:        &generatedAccount,
-			IncrementSequenceNum: true,
-			Operations:           []txnbuild.Operation{&paymentOp},
-			BaseFee:              txnbuild.MinBaseFee,
-			Timebounds:           txnbuild.NewTimeout(300),
-		},
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Sign the transaction with the secret hash.
-	tx, err = tx.Sign(network.TestNetworkPassphrase, generatedKp)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Send the transaction to the network.
-	status, err := client.SubmitTransaction(tx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Print the response.
-	fmt.Printf("Successfully submitted transaction!\nTransaction ID: %v\n", status.ID)
-	//----------------------------------CHEAT-END-------------------------------------
-
-	// Build a revoke sponsorship operation for the sponsored account.
-	revokeAccOp := txnbuild.RevokeSponsorship{
-		SponsorshipType: txnbuild.RevokeSponsorshipTypeAccount,
-		Account:         &sponsoredAccount,
-	}
-
-	// Build a revoke sponsorship operation for the sponsored signer.
-	revokeSignOp := txnbuild.RevokeSponsorship{
-		SponsorshipType: txnbuild.RevokeSponsorshipTypeSigner,
-		Signer: &txnbuild.SignerID{
-			AccountID:     sponsoredAccount,
-			SignerAddress: sponsoredSigner,
-		},
+	// Build an account merge operation to merge the sponsored account.
+	mergeSponsoredOp := txnbuild.AccountMerge{
+		Destination:   public,
+		SourceAccount: sponsoredAccount,
 	}
 
 	// Build an account merge operation.
@@ -167,11 +100,11 @@ func main() {
 	}
 
 	// Construct the transaction.
-	tx, err = txnbuild.NewTransaction(
+	tx, err := txnbuild.NewTransaction(
 		txnbuild.TransactionParams{
 			SourceAccount:        &questAccount,
 			IncrementSequenceNum: true,
-			Operations:           []txnbuild.Operation{&revokeAccOp, &revokeSignOp, &mergeOp},
+			Operations:           []txnbuild.Operation{&mergeSponsoredOp, &mergeOp},
 			BaseFee:              txnbuild.MinBaseFee,
 			Timebounds:           txnbuild.NewTimeout(300),
 		},
